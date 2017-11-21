6b6404ab9753a0979ca1eacb3a5725903779177a
Vestergaard <Vestergaard32@gmail.com>
2017-11-21 13:49:42 +0100

    Working angular app in visual studio

--- DotNetCoreFitnessApp/FitnessApp/src/app/app.component.ts
+++ DotNetCoreFitnessApp/FitnessApp/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit() {
+  constructor(private _httpService: Http) { }
+
   title = 'Angular Fitness App';
+  apiValues: string[] = [];
+  ngOnInit() {
+    this._httpService.get('/api/derp').subscribe(values => {
+      this.apiValues = values.json() as string[];
+    });
+  }
 }
--- DotNetCoreFitnessApp/FitnessApp/src/app/app.module.ts
+++ DotNetCoreFitnessApp/FitnessApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AuthInterceptor } from './auth-interceptor'
 import { LoginModule } from './login/login.module';
 import { WorkoutModule } from './workout/workout.module';
@@ -18,7 +19,8 @@ import { LoginService } from './login/login.service'
     HttpClientModule,
     BrowserModule,
     LoginModule,
-    WorkoutModule
+    WorkoutModule,
+    FormsModule
   ],
   providers: [
     FitnessApiService, 
