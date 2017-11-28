3219f3d773efc89b9fef221bda90f7ee9885e98f
Daniel <vestergaard32@gmail.com>
2017-11-29 00:13:00 +0100

    Added get all workoutprograms functionality

---
+++ DotNetCoreFitnessApp/Controllers/WorkoutController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreFitnessApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreFitnessApp.Controllers
+{
+    [Route("api/Workouts")]
+    public class WorkoutController : Controller
+    {
+        private readonly IWorkoutRepository _workoutRepository;
+        
+        public WorkoutController(IWorkoutRepository workoutRepository)
+        {
+            _workoutRepository = workoutRepository;
+        }
+        
+        [HttpGet]
+        public IActionResult GetAllWorkouts()
+        {
+            var workouts = _workoutRepository.GetAllWorkouts();
+            return Ok(workouts);
+        }
+    }
+}
